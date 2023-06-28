6fc7db194672e7ea2a5f871f30334100e6000673
YeisonRamirez20 <yeisonramirez@wposs.com>
2023-06-28 15:12:49 -0500

    IMPLEMENTACION - Se agrega código PHP para obtener la hora actual de un servidor NTP especificado. Los cambios incluyen:
    *) Configurar el servidor NTP y el puerto en el código.
    *) Crear un socket UDP y enviar una solicitud al servidor NTP.
    *) Recibir y procesar la respuesta del servidor NTP para obtener la hora actual.
    *) Mostrar la hora actual en una página HTML junto con una imagen del sistema.
    *) Agregar estilos CSS para mejorar la apariencia de la página.

--- ConfiguracionNTPServer.php
+++ ConfiguracionNTPServer.php
@@ -1,13 +1,27 @@
 <?php
 
+//Nota si quieres consumir otro servidor NTP, configura $servidorNT y date_default_timezone_set('EL que quieras');
+/*
+"UTC": Para la Coordinated Universal Time (Tiempo Universal Coordinado).
+"Europe/Paris": Para la zona horaria de París en Europa.
+"Asia/Shanghai": Para la zona horaria de Shanghái en Asia.
+"Australia/Sydney": Para la zona horaria de Sídney en Australia.
+"Africa/Johannesburg": Para la zona horaria de Johannesburgo en África.
+"America/New_York": Para la zona horaria de Nueva York en América del Este.
+"Europe/London": Para la zona horaria de Londres en Europa.
+"Asia/Tokyo": Para la zona horaria de Tokio en Asia.
+America/Bogota: Para la zona horaria de Bogota en America.
+*/
+
 // Dirección IP o nombre de dominio del servidor NTP
-$servidorNTP = '192.168.1.8';
+$servidorNTP = '0.south-america.pool.ntp.org';
 
 // Puerto NTP
 $puertoNTP = 123;
 
 // Función para refrescar la página cada segundo
-function refrescarPagina() {
+function refrescarPagina()
+{
     header("Refresh: 1");
 }
 
@@ -39,9 +53,10 @@ socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array('sec' => 5, 'usec' => 
 
 // Recibe la respuesta del servidor NTP
 $respuestaNTP = '';
-$resultado = socket_recv($socket, $respuestaNTP, 48, 0);
 
-if ($resultado === false) {
+try {
+    $resultado = socket_recv($socket, $respuestaNTP, 48, 0);
+} catch (Exception $e) {
     // Error al recibir la respuesta
     die("Error al recibir la respuesta NTP");
 }
@@ -58,52 +73,111 @@ $horaActual = '';
 if ($respuestaNTP !== '') {
     // El formato de respuesta NTP es un timestamp de 64 bits que consta de 2 partes: segundos y fracciones de segundos
     $ntpTimestamp = unpack('N12', $respuestaNTP);
-    
+
     // Obtiene los segundos desde 1900 (epoch de NTP)
     $segundos = $ntpTimestamp[9] - 2208988800;
-    
+
     // Convierte los segundos a una fecha y hora legible
     $horaActual = date('Y-m-d H:i:s', $segundos);
 }
 
-// Genera el HTML para mostrar la hora actual
-$html = <<<HTML
+try {
+    refrescarPagina();
+} catch (Exception $e) {
+    // Manejo del error al refrescar la página
+    echo "Error al refrescar la página";
+}
+?>
+
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>Hora del servidor NTP</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/css/bootstrap.min.css">
     <style>
+        /* Estilos CSS */
+
         body {
             font-family: Arial, sans-serif;
             text-align: center;
             padding-top: 50px;
+            background-color: #F2F2F2;
         }
 
-        h1 {
-            font-size: 140px;
+        .container {
+            max-width: 700px;
+            margin: 0 auto;
+            background-color: #FFFFFF;
+            border-radius: 10px;
+            padding: 20px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-wrap: wrap;
         }
 
-        h3 {
+        .display-1 {
             font-size: 60px;
+            color: #FF4500;
+            margin-bottom: 30px;
         }
 
-        p {
-            font-size: 80px;
+        .footer-text {
+            font-size: 24px;
+            color: #000000;
+            margin-top: 50px;
         }
 
+        .widget {
+            background-color: #FFA500;
+            color: #FFFFFF;
+            padding: 10px;
+            margin-bottom: 20px;
+            border-radius: 5px;
+            width: 100%;
+        }
+
+        .widget-heading {
+            font-size: 30px;
+            margin-bottom: 10px;
+        }
+
+        .widget-content {
+            font-size: 24px;
+        }
+
+        .system-image {
+            width: 200px;
+            height: auto;
+            margin-left: 50px;
+            border-radius: 10px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        }
     </style>
 </head>
+
 <body>
-    <h1>Hora del servidor NTP</h1>
+    <div class="container">
+        <div>
+            <h1 class="display-1">Hora del servidor NTP</h1>
 
-    <p>{$horaActual}</p>
+            <div class="widget">
+            </div>
+            <img src="https://th.bing.com/th/id/OIP.7djNy7VW1M4gDVAXskfBMgHaD5?pid=ImgDet&rs=1" alt="Imagen del sistema">
+            <div class="widget">
+            </div>
 
-    <h3>Creado por Yeison Ramirez</h3>
+            <p style="font-size: 25px; color: #FF4500;">Fecha y hora: <span style="font-size: 25px; color: #000000;"><?php echo $horaActual; ?></span></p>
+            <!-- <p style="font-size: 25px; color: #FF4500;">Hora actual del sistema: <span style="font-size: 25px; color: #000000;"><?php echo date('Y-m-d H:i:s'); ?></span></p> -->
+        </div>
+        
+        <div class="footer">
+            <p class="footer-text">® Creado por Yeison Ramirez</p>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0/js/bootstrap.bundle.min.js"></script>
 </body>
-</html>
-HTML;
 
-// Imprime el HTML y refresca la página cada segundo
-echo $html;
-refrescarPagina();
-?>
+</html>
